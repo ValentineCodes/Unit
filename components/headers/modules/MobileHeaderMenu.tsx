7db4aed028e32ca1925a909d0ed4c69c2915a3b2
ValentineCodes <Valentineorga@gmail.com>
2023-02-03 16:03:22 -0800

    modified header menu

--- components/headers/modules/MobileHeaderMenu.tsx
+++ components/headers/modules/MobileHeaderMenu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import {
   UserIcon,
   WalletIcon,
@@ -6,7 +6,7 @@ import {
   Bars3Icon,
   XMarkIcon,
 } from "@heroicons/react/24/solid";
-import { Menu } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
 
 type Props = {};
 
@@ -23,48 +23,58 @@ function MobileHeaderMenu({}: Props) {
             )}
           </Menu.Button>
 
-          <Menu.Items
-            as="ul"
-            className="absolute bg-white shadow-lg rounded-md text-md flex flex-col items-start w-[100vw] left-0 top-[8%] pb-5"
+          <Transition
+            as={Fragment}
+            enter="transition duration-300 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
           >
-            <Menu.Item
-              as="li"
-              className="hover:bg-[#8258FC] hover:text-white transition-colors duration-500 p-5 w-full"
+            <Menu.Items
+              as="ul"
+              className="absolute bg-[#f8fcff] shadow-lg rounded-md text-sm flex flex-col items-start w-[100vw] left-0 top-[8%] pb-5"
             >
-              Explore
-            </Menu.Item>
-            <Menu.Item
-              as="li"
-              className="hover:bg-[#8258FC] hover:text-white transition-colors duration-500 p-5 w-full"
-            >
-              Stats
-            </Menu.Item>
-            <Menu.Item
-              as="li"
-              className="hover:bg-[#8258FC] hover:text-white transition-colors duration-500 p-5 w-full"
-            >
-              Resources
-            </Menu.Item>
-            <Menu.Item
-              as="li"
-              className="hover:bg-[#8258FC] hover:text-white transition-colors duration-500 p-5 w-full"
-            >
-              Create
-            </Menu.Item>
-            <Menu.Item as="li" className="flex items-center space-x-5 pl-5">
-              <div className="bg-white shadow-lg rounded-full p-2">
-                <WalletIcon className="w-5 h-5" color="black" />
-              </div>
+              <Menu.Item
+                as="li"
+                className="hover:bg-[#8258FC] hover:text-white transition-colors duration-500 p-5 w-full"
+              >
+                Explore
+              </Menu.Item>
+              <Menu.Item
+                as="li"
+                className="hover:bg-[#8258FC] hover:text-white transition-colors duration-500 p-5 w-full"
+              >
+                Stats
+              </Menu.Item>
+              <Menu.Item
+                as="li"
+                className="hover:bg-[#8258FC] hover:text-white transition-colors duration-500 p-5 w-full"
+              >
+                Resources
+              </Menu.Item>
+              <Menu.Item
+                as="li"
+                className="hover:bg-[#8258FC] hover:text-white transition-colors duration-500 p-5 w-full"
+              >
+                Create
+              </Menu.Item>
+              <Menu.Item as="li" className="flex items-center space-x-5 pl-5">
+                <div className="bg-white shadow-lg rounded-full p-2">
+                  <WalletIcon className="w-5 h-5" color="black" />
+                </div>
 
-              <div className="bg-white shadow-lg rounded-full p-2">
-                <UserIcon className="w-5 h-5" color="black" />
-              </div>
+                <div className="bg-white shadow-lg rounded-full p-2">
+                  <UserIcon className="w-5 h-5" color="black" />
+                </div>
 
-              <div className="bg-white shadow-lg rounded-full p-2">
-                <MoonIcon className="w-5 h-5" color="black" />
-              </div>
-            </Menu.Item>
-          </Menu.Items>
+                <div className="bg-white shadow-lg rounded-full p-2">
+                  <MoonIcon className="w-5 h-5" color="black" />
+                </div>
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
         </>
       )}
     </Menu>
